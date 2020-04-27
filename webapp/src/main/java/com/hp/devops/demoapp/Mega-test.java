7cb8ceb75b31e16d29958bd1ccd0934572019eed
MemeRO <muresanu.mihai@gmail.com>
2020-04-27 16:48:08 +0300

    Update and rename mega-test.java to Mega-test.java

--- webapp/src/main/java/com/hp/devops/demoapp/Mega-test.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Mega-test.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 
-public class Calcs {
+public class Mega-test {
 
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
@@ -25,11 +25,6 @@ public class Calcs {
 	public static boolean alwaysFalse() {
 		return false;
 	}
-}
-
-
-public class Calcs2 {
-
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
 	}
@@ -52,11 +47,6 @@ public class Calcs2 {
 	public static boolean alwaysFalse() {
 		return false;
 	}
-}
-
-
-public class Calcs3 {
-
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
 	}
@@ -79,10 +69,6 @@ public class Calcs3 {
 	public static boolean alwaysFalse() {
 		return false;
 	}
-}
-
-public class Calcs4 {
-
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
 	}
@@ -105,11 +91,50 @@ public class Calcs4 {
 	public static boolean alwaysFalse() {
 		return false;
 	}
-}
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
 
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
 
-public class Calcs5 {
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
 
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
 	}
