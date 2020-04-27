458b5b43469255360cdf25d3250baa93d0b24938
MemeRO <muresanu.mihai@gmail.com>
2020-04-27 16:45:24 +0300

    Update mega-test.java

--- webapp/src/main/java/com/hp/devops/demoapp/mega-test.java
+++ webapp/src/main/java/com/hp/devops/demoapp/mega-test.java
@@ -1,1 +1,135 @@
+package com.hp.devops.demoapp;
 
+
+public class Calcs {
+
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+}
+
+
+public class Calcs2 {
+
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+}
+
+
+public class Calcs3 {
+
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+}
+
+public class Calcs4 {
+
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+}
+
+
+public class Calcs5 {
+
+	public static int sum(int a, int b) {     //  1 test covers this
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {     //  3 tests cover this
+		return a - b;
+	}
+
+	public static boolean equal(int a, int b) {     //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
+		return (a == b);
+	}
+
+	public static boolean alwaysTrue() {
+		return true;
+	}
+	public static boolean alwaysFalse() {
+		return false;
+	}
+}
