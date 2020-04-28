6fbe6f69a4274035c7b6eadbbe477e0d13c4ee4e
muresanu <mihai.muresanu@hpe.com>
2020-04-28 11:59:30 +0300

    before

--- webapp/src/test/java/com/hp/devops/demoapp/megaTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/megaTest.java
@@ -267,4 +267,259 @@ public class megaTest {
 	@Test
 	public void always_false_A4() {assertTrue(Calcs.alwaysTrue());}
 
+	@Test
+	public void sum_test_A5() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
+
+	@Test
+	public void sub_test_A5() {
+		assertEquals(4, Calcs.sub(5, 1));
+	}
+
+	@Test
+	public void sub_test_B5() {
+		assertEquals(2, Calcs.sub(-1, -3));
+	}
+
+	@Test
+	public void sub_test_C5() {
+		assertEquals(3, Calcs.sub(10, 7));
+	}
+
+	@Test
+	public void eq_test_A5() {
+		assertTrue(Calcs.equal(1, 1));
+	}
+
+	@Test
+	public void eq_test_B5() {
+		assertTrue(Calcs.equal(-1, -1));
+	}
+
+	@Test
+	public void neq_test_A5() {
+		assertTrue(Calcs.nonEqual(0, 1));
+	}
+
+	@Test
+	public void neq_test_B5() {
+		assertTrue(Calcs.nonEqual(-1, 0));
+	}
+
+	@Test
+	public void neq_test_C_BrokenTest5() {
+		assertTrue(Calcs.nonEqual(5, 5));
+	}
+
+	@Test
+	public void always_true_A5() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void always_false_A5() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void sum_test_A6() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
+
+	@Test
+	public void sub_test_A6() {
+		assertEquals(4, Calcs.sub(5, 1));
+	}
+
+	@Test
+	public void sub_test_B6() {
+		assertEquals(2, Calcs.sub(-1, -3));
+	}
+
+	@Test
+	public void sub_test_C6() {
+		assertEquals(3, Calcs.sub(10, 7));
+	}
+
+	@Test
+	public void eq_test_A6() {
+		assertTrue(Calcs.equal(1, 1));
+	}
+
+	@Test
+	public void eq_test_B6() {
+		assertTrue(Calcs.equal(-1, -1));
+	}
+
+	@Test
+	public void neq_test_A6() {
+		assertTrue(Calcs.nonEqual(0, 1));
+	}
+
+	@Test
+	public void neq_test_B6() {
+		assertTrue(Calcs.nonEqual(-1, 0));
+	}
+
+	@Test
+	public void neq_test_C_BrokenTest6() {
+		assertTrue(Calcs.nonEqual(5, 5));
+	}
+
+	@Test
+	public void always_true_A6() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void always_false_A6() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void sum_test_A7() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
+
+	@Test
+	public void sub_test_A7() {
+		assertEquals(4, Calcs.sub(5, 1));
+	}
+
+	@Test
+	public void sub_test_B7() {
+		assertEquals(2, Calcs.sub(-1, -3));
+	}
+
+	@Test
+	public void sub_test_C7() {
+		assertEquals(3, Calcs.sub(10, 7));
+	}
+
+	@Test
+	public void eq_test_A7() {
+		assertTrue(Calcs.equal(1, 1));
+	}
+
+	@Test
+	public void eq_test_B7() {
+		assertTrue(Calcs.equal(-1, -1));
+	}
+
+	@Test
+	public void neq_test_A7() {
+		assertTrue(Calcs.nonEqual(0, 1));
+	}
+
+	@Test
+	public void neq_test_B7() {
+		assertTrue(Calcs.nonEqual(-1, 0));
+	}
+
+	@Test
+	public void neq_test_C_BrokenTest7() {
+		assertTrue(Calcs.nonEqual(5, 5));
+	}
+
+	@Test
+	public void always_true_A7() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void always_false_A7() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void sum_test_A8() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
+
+	@Test
+	public void sub_test_A8() {
+		assertEquals(4, Calcs.sub(5, 1));
+	}
+
+	@Test
+	public void sub_test_B8() {
+		assertEquals(2, Calcs.sub(-1, -3));
+	}
+
+	@Test
+	public void sub_test_C8() {
+		assertEquals(3, Calcs.sub(10, 7));
+	}
+
+	@Test
+	public void eq_test_A8() {
+		assertTrue(Calcs.equal(1, 1));
+	}
+
+	@Test
+	public void eq_test_B8() {
+		assertTrue(Calcs.equal(-1, -1));
+	}
+
+	@Test
+	public void neq_test_A8() {
+		assertTrue(Calcs.nonEqual(0, 1));
+	}
+
+	@Test
+	public void neq_test_B8() {
+		assertTrue(Calcs.nonEqual(-1, 0));
+	}
+
+	@Test
+	public void neq_test_C_BrokenTest8() {
+		assertTrue(Calcs.nonEqual(5, 5));
+	}
+
+	@Test
+	public void always_true_A8() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void always_false_A8() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void sum_test_A9() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
+
+	@Test
+	public void sub_test_A9() {
+		assertEquals(4, Calcs.sub(5, 1));
+	}
+
+	@Test
+	public void sub_test_B9() {
+		assertEquals(2, Calcs.sub(-1, -3));
+	}
+
+	@Test
+	public void sub_test_C9() {
+		assertEquals(3, Calcs.sub(10, 7));
+	}
+
+	@Test
+	public void eq_test_A9() {
+		assertTrue(Calcs.equal(1, 1));
+	}
+
+	@Test
+	public void eq_test_B9() {
+		assertTrue(Calcs.equal(-1, -1));
+	}
+
+	@Test
+	public void neq_test_A9() {
+		assertTrue(Calcs.nonEqual(0, 1));
+	}
+
+	@Test
+	public void neq_test_B9() {
+		assertTrue(Calcs.nonEqual(-1, 0));
+	}
+
+	@Test
+	public void neq_test_C_BrokenTest9() {
+		assertTrue(Calcs.nonEqual(5, 5));
+	}
+
+	@Test
+	public void always_true_A9() {assertTrue(Calcs.alwaysTrue());}
+
+	@Test
+	public void always_false_A9() {assertTrue(Calcs.alwaysTrue());}
+
 }
