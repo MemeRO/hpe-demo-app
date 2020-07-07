74fe4020de8b6681ae96d0eb7f418ab1f468631a
MemeRO <muresanu.mihai@gmail.com>
2020-07-07 12:30:13 +0300

    Create megaTest2.java

---
+++ webapp/src/test/java/com/hp/devops/demoapp/megaTest2.java
@@ -0,0 +1,15 @@
+package com.hp.devops.demoapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class megaTest2 {
+
+
+	@Test
+	public void sum_test_A() {
+		assertEquals(3, Calcs.sum(1,5));
+	}
